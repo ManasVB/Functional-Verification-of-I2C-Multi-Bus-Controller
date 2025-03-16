ec49ddb214f9b2494bb6eed723237ecdfb9ef2d9
ManasVB <mbuzruk09@gmail.com>
2025-03-16 19:34:22 -0400

    i2c_pkg-create-1

---
+++ proj2/lab_1_provided_files/ece745_projects/verification_ip/interface_packages/i2c_pkg/i2c_pkg.sv
@@ -0,0 +1,13 @@
+package i2c_pkg;
+
+	import ncsu_pkg::*;
+
+ 	`include "ncsu_macros.svh"
+ 	
+ 	`include "src/i2c_configuration.svh"
+ 	`include "src/i2c_agent.svh"
+ 	`include "src/i2c_driver.svh"
+ 	`include "src/i2c_monitor.svh"
+ 	`include "src/i2c_transaction.svh" 
+endpackage
+
