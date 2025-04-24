5ad63801c5d3abe5620b22ce3c4ae6f5b2a0678f
ManasVB <mbuzruk09@gmail.com>
2025-04-24 14:18:18 -0400

    save

--- project_benches/proj4/sim/regress.sh
+++ project_benches/proj4/sim/regress.sh
@@ -2,12 +2,15 @@
 
 make clean compile optimize
 
+# Run Register Tests
 make run_cli GEN_TRANS_TYPE=i2cmb_register_test
 mv transcript transcript_register_test
 
+# Run Dut Functionality Tests
 make run_cli GEN_TRANS_TYPE=i2cmb_dut_functionality_test
 mv transcript transcript_i2c_operation
 
+# Run Compulsory Tests
 make run_cli GEN_TRANS_TYPE=i2cmb_generator_compulsory_test
 mv transcript transcript_generator_compulosry_test
 
--- verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_dut_functionality_test.svh
+++ verification_ip/environment_packages/i2cmb_env_pkg/src/i2cmb_dut_functionality_test.svh
@@ -1,35 +1,44 @@
 class i2cmb_dut_functionality_test extends generator;
 
-  function new (string name = "", ncsu_component_base parent = null);
-      super.new (name, parent);
-  endfunction
+    function new (string name = "", ncsu_component_base parent = null);
+        super.new (name, parent);
+    endfunction
 
-  local wb_agent agent_wb;
+    local wb_agent agent_wb;
 
-  protected task generate_wb_transaction_write (bit [1:0] wb_addr, bit [7:0] wb_data); 
-      wb_transaction_base wb_trans = new ("wb_transaction");
-      wb_trans.wb_addr = wb_addr;
-      wb_trans.wb_data = wb_data;
-      agent_wb.bl_put (wb_trans);
-  endtask
+    protected task generate_wb_transaction_write (bit [1:0] wb_addr, bit [7:0] wb_data); 
+        wb_transaction_base wb_trans = new ("wb_transaction");
+        wb_trans.wb_addr = wb_addr;
+        wb_trans.wb_data = wb_data;
+        agent_wb.bl_put (wb_trans);
+    endtask
 
-  protected task generate_wb_transaction_read (bit [1:0] wb_addr, ref bit [7:0] wb_data);
-      wb_transaction_base wb_trans = new ("wb_transaction");
-      wb_trans.wb_addr = wb_addr;
-      // agent_wb.bl_get_ref (wb_trans);
-      wb_data = wb_trans.wb_data;
-  endtask
-  
-  // Testplan 2.1: Ensure that the `BUS ID` field in the CSR matches the selected bus ID
-  local task test_bus_id ();
-      bit [7:0] csr_value;
+    protected task generate_wb_transaction_read (bit [1:0] wb_addr, ref bit [7:0] wb_data);
+        wb_transaction_base wb_trans = new ("wb_transaction");
+        wb_trans.wb_addr = wb_addr;
+        // agent_wb.bl_get_ref (wb_trans);
+        wb_data = wb_trans.wb_data;
+    endtask
+    
 
-      for (byte i = 4'd0; i < 4'd16; i++) begin 
-          generate_wb_transaction_write (DPR_ADDR, i);
-          generate_wb_transaction_write (CMDR_ADDR, CMD_SET_BUS);
-          // generate_wb_transaction_read (CSR_ADDR, csr_value);
-          // assert (csr_value[3:0] == i);
-      end
-  endtask
+    virtual task run();
+        /* Test Plan - 2.5: CSR Bus ID Test
+        * Check if BUS ID field in CSR matches selected BUS ID 
+        */
+        $display("************TESTPLAN 2.5: CSR Bus ID Test Start *************");
+
+
+        $display("************TESTPLAN 2.5: CSR Bus ID Test Done *************");
+
+        
+        /* Test Plan - 2.8: Acknowledge detection
+        * Check if DUT is detecting ack during write operation 
+        */
+        $display("************TESTPLAN 2.8: Acknowledge Detection Start *************");
+
+
+        $display("************TESTPLAN 2.8: Acknowledge Detection Done *************");
+
+    endtask
 
 endclass
